1a8e507e03768de9a6a6e39534e28c693707b8b0
delyteja <tejamukti03@gmail.com>
2018-12-06 02:25:41 +0700

    edit index.php

--- index.php
+++ index.php
@@ -28,41 +28,48 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        <form >
+      <div class="modal-body ">
+        <form style="margin-left:35px;">
         <input type="hidden" name="id_vidio" id="id_vidio" value="">
+          
           <div class="form-group">
-            <label for="format">Output Format</label>
-            <select id="format" name="format">
-              <option value="">Please select output format...</option>
-              <option value="mp4">MP4</option>
-              <option value="mkv">MKV</option>
-              <option value="avi">AVI</option>
-            </select>
+            <label for="format" class="col-md-4 control-label">Output Format :</label>
+            <div class="col-md-7">
+                <select  class="form-control" id="format" name="format">
+                  <option value="">Please select output format...</option>
+                  <option value="mp4">MP4</option>
+                  <option value="mkv">MKV</option>
+                  <option value="avi">AVI</option>
+                </select>
+            </div>
           </div>
           
           <div class="form-group">
-          <label for="frame_size">Resolution</label>
-          <select id="frame_size" name="frame_size" required>
-            <option value="" >Please select frame size</option>
-            <option value="426x240">426x240</option>
-            <option value="480x360">480x360</option>
-            <option value="680x480">680x480</option>
-            <option value="1280x720">1280x720</option>
-          </select>
+          <label for="frame_size" class="col-md-4">Resolution:</label>
+            <div class="col-md-7">
+              <select class="form-control" id="frame_size" name="frame_size" required>
+                <option value="" >Please select frame size</option>
+                <option value="426x240">426x240</option>
+                <option value="480x360">480x360</option>
+                <option value="680x480">680x480</option>
+                <option value="1280x720">1280x720</option>
+              </select>
+            </div>
           </div>
-          
+
           <div class="form-group">
-            <label for="framerate">Frame Rate</label>
-            <select id="framerate" name="framerate">
-              <option value="" selected="selected">Not Determined</option>
-              <option value=" -r 24 ">24 fps</option>
-              <option value=" -r 25 ">25 fps</option>
-              <option value=" -r 30 ">30 fps</option>
-              <option value=" -r 48 ">48 fps</option>
-              <option value=" -r 50 ">50 fps</option>
-              <option value=" -r 60 ">60 fps</option>
-            </select>
+            <label for="framerate" class="col-md-4">Frame Rate :</label>
+            <div class="col-md-7">
+              <select class="form-control"  id="framerate" name="framerate">
+                <option value="" selected="selected">Not Determined</option>
+                <option value=" -r 24 ">24 fps</option>
+                <option value=" -r 25 ">25 fps</option>
+                <option value=" -r 30 ">30 fps</option>
+                <option value=" -r 48 ">48 fps</option>
+                <option value=" -r 50 ">50 fps</option>
+                <option value=" -r 60 ">60 fps</option>
+              </select>
+            </div>
           </div>
           
         </form>
@@ -312,13 +319,14 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Download Selesai</h5>
+        <h5 class="modal-title text-center" id="exampleModalLongTitle">Siap Download</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
+        
       </div>
-      <div class="modal-body">
-      <a href="http://dummylink" download><button class="green">Download</button></a>        
+      <div class="modal-body text-center">
+      <a href="http://dummylink" download><button class="btn btn-primary">Download</button></a>        
       </div>
     </div>
   </div>
